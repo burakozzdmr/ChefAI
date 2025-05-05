6c965c5a93c4ea1158701ec4c2f464780c25f076
Burak Özdemir <mburak.ozdemir03@gmail.com>
2025-05-05 15:42:59 +0300

    Edit: network manager debug prints is deleted

--- ChefAI/Network/Managers/NetworkManager.swift
+++ ChefAI/Network/Managers/NetworkManager.swift
@@ -27,27 +27,17 @@ extension NetworkManager: NetworkManagerProtocol {
     func sendRequest<T: Codable>(request: URLRequest, as: T.Type, completion: @escaping (Result<T, NetworkError>) -> Void) {
         session.dataTask(with: request) { data, response, error in
             if error != nil {
-                print("network manager error var")
                 completion(.failure(.invalidURL))
                 return
             }
             
-            if let httpResponse = response as? HTTPURLResponse {
-                print("httpResponse hata var network manager da")
-                print(httpResponse.statusCode)
-            } else {
-                print("httpResponse hata yok devam ediyor -networkManager")
-            }
-            
             guard let data = data else {
-                print("data boş geldi - networkManager")
                 completion(.failure(.emptyDataError))
                 return
             }
             
             do {
                 let jsonResponse = try JSONDecoder().decode(T.self, from: data)
-                print("network manager decode başarılı")
                 completion(.success(jsonResponse))
             } catch {
                 print("JSON DECODE ERROR: \(error.localizedDescription)")
